216a7782728456752516db02e25b33543927bf80
ArechuaDaniel <daniel.ar98hotmail.com>
2022-09-14 23:07:44 -0500

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Memorize } from './06-memos/Memorize'
+//import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/07-tarea-memo/Padre'
+//import { MemoHook } from './06-memos/MemoHook'
+//import { Memorize } from './06-memos/Memorize'
 //import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
 //import {MultipleCustomHooks} from './03-examples/MultipleCustomHooks'
 //import { FocusScreen } from './04-useRef/FocusScreen'
@@ -13,6 +16,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <Memorize />
+    <Padre />
   
 )
